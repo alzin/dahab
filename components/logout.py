2cd48a7e3c197a825fd0fff7fc1bd36c3d05e2d3
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2024-02-27 01:10:34 +0900

    add logout icon

--- components/logout.py
+++ components/logout.py
@@ -1,8 +1,35 @@
 import streamlit as st
+import streamlit_antd_components as sac
 
 
 def logout(auth_service):
-    if st.sidebar.button("Logout"):
+    st.sidebar.markdown('---')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    st.sidebar.markdown('#')
+    with st.sidebar.container():
+        logout_clicked = sac.buttons(
+            [sac.ButtonsItem(icon=sac.BsIcon(name='box-arrow-left', size=20))],
+            align='left',
+            variant='text',
+            index=None
+        )
+
+    if logout_clicked is not None:
         auth_service.sign_out()
         st.session_state.user = None
         st.session_state.requirements_submitted = False
